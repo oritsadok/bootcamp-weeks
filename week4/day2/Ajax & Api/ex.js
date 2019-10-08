b1859dc97caf71eb18e05d6cd7ee8b25fee671ec
ori <oritsadok1@gmail.com>
2019-10-08 16:27:24 +0300

    upload

---
+++ week4/day2/Ajax & Api/ex.js
@@ -0,0 +1,119 @@
+//ex1
+// const fetch= function(isbn){
+// $.ajax({
+//     method: "GET",
+//     url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn,
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error:  function (xhr, text, error) {
+//         console.log(text);
+//     }
+// }); 
+// }
+
+// fetch(9782806269171)
+
+
+//ex2
+
+
+// const fetch= function(queryType,queryValue){
+// $.ajax({
+//     method: "GET",
+//     url: 'https://www.googleapis.com/books/v1/volumes?q='+queryType+":"+queryValue,
+//     success: function (data) {
+//         console.log(data);
+//     },
+//     error:  function (xhr, text, error) {
+//         console.log(text);
+//     }
+// }); 
+// }
+
+// fetch("title","How to Win Friends and influence people")
+
+
+
+
+//ex3
+
+// const fetch= function(queryType,queryValue){
+// $.ajax({
+//     method: "GET",
+//     url: 'https://www.googleapis.com/books/v1/volumes?q='+queryType+":"+queryValue,
+//     success: function (data) {
+//        const info =  data.items
+//    let Fdata =  info.map(i=>{return{title:i.volumeInfo.title,
+//         authors:i.volumeInfo.authors
+//         ,isbn:i.volumeInfo.industryIdentifiers[0].identifier}})
+
+//        console.log(Fdata);
+//     },
+//     error:  function (xhr, text, error) {
+//         console.log(text);
+//     }
+// }); 
+// }
+
+// fetch("title","How to Win Friends and influence people")
+
+
+//ex4
+// const fetch= function(){
+// $.ajax({
+//     method:"GET",
+//     url: "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=miXO68AAl81ztbX1K4hZXcvwM80Sfz2w",
+//      success: function (Data) {
+//          let Gdata = Data.data[0].url
+//         $("body").append(`<iframe src=`+Gdata+ `>`+`</iframe>`)
+//          console.log(Gdata);
+//      },
+//     error:  function (xhr, text, error) {
+//          console.log(text);
+
+//  }
+// }); 
+// }
+
+// fetch()
+
+
+//ex5
+
+// $("body").append("<input ></input>")
+// $("body").append("<button>click me</button>")
+
+// const fetch = function (subject) {
+//     $.ajax({
+//         method: "GET",
+//         url: "http://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=miXO68AAl81ztbX1K4hZXcvwM80Sfz2w",
+//         success: function (Data) {
+//             let Gdata = Data.data[0].url
+//             $("body").append(`<iframe src=` + Gdata + `>` + `</iframe>`)
+//             console.log(Gdata);
+//         },
+//         error: function (xhr, text, error) {
+//             console.log(text);
+            
+//         }
+//     });
+// }
+
+// $("button").on("click", function(){
+//     let momo = $("input").val()
+//     fetch(momo)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
--- week4/day2/Ajax & Api/lesson.js
+++ week4/day2/Ajax & Api/lesson.js
@@ -1,8 +1,64 @@
-$.get("https://www.googleapis.com/books/v1/volumes?q=title:Name of the Wind", function(result){
-    console.log(result.items[0].volumeInfo.description)//finding the 
- })
+// $.get("https://www.googleapis.com/books/v1/volumes?q=title:Name of the Wind", function(result){
+//     console.log(result.items[0].volumeInfo.description)//finding the first description of the first item 
+//  })
 
 
 
 
+// //request the data with a GET request
+// $.get("https://jsonplaceholder.typicode.com/users", function(users){
+//   //extract the geo data of the first user
+//   let lat = users[0].address.geo.lat
+//   let long = users[0].address.geo.lng
+  
+//    $("body").append(`<div>${lat}-${long}</div>`)
 
+//   //log the data 
+//   console.log(`${lat}, ${long}`) //prints -37.3159, 81.1496
+// })
+
+
+// $.get("https://jsonplaceholder.typicode.com/users", function(users){
+//  console.log(users[users.length-1].company.catchPhrase)
+   
+
+// })
+
+
+
+// $.ajax({
+//     method: "GET",
+//     url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+//     success: function(data) {
+//       console.log(data);
+//     }
+//   })
+
+
+
+
+// $.ajax({
+//     method:"GET",
+//     url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521',
+//     success: userData=function(data){
+//         console.log(data)
+//     }
+// })
+
+
+
+//ex1
+const fetch= function(isbn){
+$.ajax({
+    method: "GET",
+    url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+isbn,
+    success: function (data) {
+        console.log(data);
+    },
+    error:  function (xhr, text, error) {
+        console.log(text);
+    }
+}); 
+}
+
+fetch(9782806269171)
